3b65da26bd46097cd73058da24b6cde8903f94cd
hahahahahanananana <hkataoka925@st1.shinagawajoshi.ed.jp>
2017-06-20 17:59:10 +0900

    20170620

--- photo/ViewController.swift
+++ photo/ViewController.swift
@@ -7,19 +7,82 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import AVFoundation
+class ViewController: UIViewController,AVCapturePhotoCaptureDelegate  {
+    
+    let captureSession: AVCaptureSession = AVCaptureSession()
+    
+    var imageOutput: AVCapturePhotoOutput = AVCapturePhotoOutput()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let devices = AVCaptureDeviceDiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: nil, position: .back).devices
+        
+        let videoInput = try? AVCaptureDeviceInput(device: devices?.first)
+        
+        captureSession.addInput(videoInput)
+        
+        captureSession.addOutput(imageOutput)
+        
+        
+        let captureVideoLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        captureVideoLayer.frame = self.view.bounds
+        captureVideoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        
+        
+        self.view.layer.insertSublayer(captureVideoLayer, at: 0)
+        
+        
+        
+        captureSession.startRunning()
     }
-
+    
+    @IBAction func cameraStart(_ sender: UIButton) {
+        
+        let setting = AVCapturePhotoSettings()
+        setting.flashMode = .auto
+        setting.isAutoStillImageStabilizationEnabled = true
+        setting.isHighResolutionPhotoEnabled = false
+        imageOutput.capturePhoto(with: setting, delegate: self)
+        captureSession.stopRunning()
+    }
+    
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        
+        if let error = error {
+            
+            print("ERROR: capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?)...\(error.localizedDescription)")
+        }
+        else {
+            
+            guard
+                let photoSampleBuffer = photoSampleBuffer,
+                let photoData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer),
+                let image = UIImage(data: photoData) else { return }
+            
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(imageWriteToSavedPhotosAlbum(_:didFinishSavingWithError:contextInfo:)), nil)
+        }
+    }
+    
+    private dynamic func imageWriteToSavedPhotosAlbum(_ image: UIImage!, didFinishSavingWithError error: Error?,  contextInfo: Any?) {
+        
+        if let error = error {
+            
+            print("ERROR: imageWriteToSavedPhotosAlbum(_:didFinishSavingWithError:contextInfo) ... \(error)")
+        }
+        else {
+            
+            
+        }
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
